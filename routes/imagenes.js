b85ac69c4b6a5bdbad66555b8a0b858570bfed99
mr_fox92 <davidalvarez54@gmail.com>
2019-05-22 19:33:21 -0400

    Seccion 14 terminada - perfil terminado

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -7,10 +7,10 @@ const fs = require('fs');
 app.get('/:tipo/:img', (req, res, next) => {
     var tipo = req.params.tipo;
     var img = req.params.img;
-    /* nos ayuda a resolver el path de la imagen por defecto, para que dicho path
+    /* path: nos ayuda a resolver el path de la imagen por defecto, para que dicho path
     siempre quede correcto. Finalmente debemos especificar toda la ruta de la imagen
     1)__dirname: me obtiene el directorio donde estoy actualmente posicionado.
-    2) hacemos referencia a la ruta relativa de la coleccion y la imagen  buscada*/
+    2) hacemos referencia a la ruta relativa de la coleccion y la imagen  buscada */
     var pathImagen = path.resolve(__dirname, `../uploads/${ tipo }/${ img }`);
     if (fs.existsSync(pathImagen)) {
         res.sendFile(pathImagen);
